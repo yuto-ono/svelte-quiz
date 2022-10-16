fed2ce42874efbd0767ab16fa4f2bec470f8e977
Yuto Ono <stellarwindmaster@gmail.com>
2022-10-17 02:29:47 +0900

    tsconfig strict true

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import "./app.scss"
 import App from "./App.svelte"
 
-type Constructor = (option: { target?: HTMLElement }) => void
+const target = document.getElementById("app")
 
-const app = new (App as unknown as Constructor)({
-  target: document.getElementById("app"),
-})
-
-export default app
+if (target != null) {
+  new App({ target })
+}
