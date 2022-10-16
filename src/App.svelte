5b2aad580d29762843f36ce4e46674c6d2ccb449
Yuto Ono <stellarwindmaster@gmail.com>
2022-10-17 01:26:07 +0900

    Scss使用

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
 </script>
 
-<main />
+<main class="container">
+  <h1 class="title">Svelte Quiz</h1>
+</main>
+
+<style lang="scss">
+  .title {
+    margin-bottom: 24px;
+    font-size: 24px;
+    font-weight: bold;
+  }
+  .container {
+    width: min(600px, 85%);
+    margin: 16px auto;
+    background-color: #fff;
+    border-radius: 4px;
+    padding: 16px;
+    position: relative;
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./app.css"
+import "./app.scss"
 import App from "./App.svelte"
 
 type Constructor = (option: { target?: HTMLElement }) => void
